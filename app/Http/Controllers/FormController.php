e6912ad7c2da8036c48e2a0041954f9b6b813eee
Paulo Jorge Candeias Machado <MJP@paulos-mbp.home>
2016-10-09 18:59:31 +0100

    finished form

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -17,9 +17,12 @@ class FormController extends Controller
 
 	public function show($id){}
 	
-   public function store(Request $request){
-   
-
+   public function store(\App\Http\Requests\FormRequest $request){
+   	$postValues = $request->all();
+   	$formModel = new \App\Form();
+   	$formModel->removeNumber($postValues['number']);
+   	$missingNumber = $formModel->getSelectedNumber();
+   	echo $missingNumber[0];
    }
  
    public function edit($id){}
