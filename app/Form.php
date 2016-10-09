919a6c5cb00f4fe5566c0f209c7229cc5955859c
Paulo Jorge Candeias Machado <MJP@paulos-mbp.home>
2016-10-09 19:00:15 +0100

    add form model

--- app/Form.php
+++ app/Form.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Form extends Model
 {
     //
+    private $contentArray = array();
+    public function __construct(){
+	   	$this->contentArray = range(1,100);
+    }
+    public function removeNumber($number){
+   		unset($this->contentArray[$number-1]);
+    }
+    public function getSelectedNumber(){
+    	   	$compareArray = range(1,100);
+    	   	$missingNumber = array_diff($compareArray,$this->contentArray);
+    	   	return array_values($missingNumber);
+    }
 }
